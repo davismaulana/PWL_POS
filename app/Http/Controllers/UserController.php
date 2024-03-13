10a5a682ca19a83cf849869f3ac5a711aeed8944
Davis Maulana Hermanto <davismaulana05@gmail.com>
2024-03-13 19:48:46 +0700

    property

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,11 +18,14 @@ class UserController extends Controller
         // UserModel::insert($data);
 
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
 
         $user = UserModel::all();
-        return view ('user', ['user' => $user]);
+        return view ('user', ['data' => $user]);
     }
 }
