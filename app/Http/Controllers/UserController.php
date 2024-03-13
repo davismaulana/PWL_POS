a2dcdd7fe2b52342092a6dece74d1d99c13b66d4
Davis Maulana Hermanto <davismaulana05@gmail.com>
2024-03-13 19:59:53 +0700

    not found exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,25 +26,31 @@ class UserController extends Controller
         // UserModel::create($data);
 
         // $user = UserModel::all();
-        // return view ('user', ['data' => $user]);
+        
 
         // $user = UserModel::find(1);
-        // return view('user', ['data' => $user]);
+       
 
         // $user = UserModel::where('level_id', 1)->first();
-        // return view('user', ['data' => $user]);
+        
 
         // $user = UserModel::firstWhere('level_id', 1);
-        // return view('user', ['data' => $user]);
+        
 
         // $user = UserModel::findOr(1,['username','nama'], function(){
         //     abort(404);
         // });
-        // return view('user', ['data' => $user]);
+       
 
-        $user = UserModel::findOr(20, ['username','nama'], function(){
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username','nama'], function(){
+        //     abort(404);
+        // });
+
+
+        // $user = UserModel::findOrFail(1);
+
+
+        $user = UserModel::where('username', 'manager')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
