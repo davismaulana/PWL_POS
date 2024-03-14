dd9fc11a661210d31b6b1ccaa7ada34a4c533e81
Davis Maulana Hermanto <davismaulana05@gmail.com>
2024-03-14 17:27:48 +0700

    Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -157,7 +157,10 @@ class UserController extends Controller
         // $user->wasChanged(['nama','username']);
         // dd($user->wasChanged('nama', 'username'));
 
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
 
     }
